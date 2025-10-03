9ad398bfd73d232d17af266a8a890344aaa675a4
asathoor <asathoor@gmail.com>
2025-10-03 05:42:48 +0200

    akvarium

--- akvarium-boilerplate/images/README.md
+++ akvarium-boilerplate/images/README.md
@@ -1,1 +1,7 @@
-# Add your images to this folder
+# Akvarium øvelse
+
+## Sources
+
+All images are from the Creative Commons domain, sources:
+
+* S![images/fish_PNG25137.png](https://pngimg.com/uploads/fish/fish_PNG25137.png)
--- akvarium-boilerplate/js/js.js
+++ akvarium-boilerplate/js/js.js
@@ -2,4 +2,32 @@
  * file: js/js.js
  * purpose: Behaviors
  **/
-console.log('Success: JavaScript running!')
+console.log('Ok, JavaScriptet kører ...')
+
+/* fiskeobjekt */
+const guldfisk = {
+    navn: "Guldfisk",
+    info: "Fisk, der stammer fra den mystiske sø Hoomhi i det nordlige Tibet.",
+    image: "fish_PNG25137_small.png",
+    xPos: 400,
+    yPos: 150,
+    speed: 12
+}
+
+/* Placer fisken på scenen */
+scene.innerHTML += `
+    <figure 
+     id="${guldfisk.navn}" 
+     onclick="fiskInfo('${guldfisk.info}')"
+     >
+        <img src="images/${guldfisk.image}" alt="${guldfisk.info}">
+    </figure>
+`
+
+/* Indsætter egenskaben "info" fra et objekt */
+function fiskInfo(tekst){
+    console.log("et klik") // test
+    info.innerHTML = "<p>" + tekst + "</p>"
+}
+
+/* Baggrundsfiskliste */
