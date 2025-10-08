e5c88393f96c30fa9089cf7bc22b73af2919ec2e
asathoor <asathoor@gmail.com>
2025-10-08 14:47:50 +0200

    iuhiuh

--- akvarie-iKLassen/js/js.js
+++ akvarie-iKLassen/js/js.js
@@ -3,9 +3,19 @@ console.log("Js kører, fedt nok!")
 function bang(){
     //alert("Hva så do?")
     document.querySelector("#info").innerHTML = `
-        <div>
+        <div onclick="badaboom()">
             <h2> Goddag med dig </h2>
             <p>  Jeg er en glad fisk, ja jeg er så. </p>
         </div>
     `
+    // her vil vi toggle
+    document.querySelector("#firkant").style.backgroundColor = "red"
+    document.querySelector("#firkant").classList.toggle("gemMig")
 }
+
+/** Her er en funktion der kaldes fra noget html-injektion */
+function badaboom(){
+    alert("Hej med dig.")
+}
+
+/**  */
