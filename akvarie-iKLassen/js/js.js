eb9dc41911868a19feb28a14aceec8d8f3684053
asathoor <asathoor@gmail.com>
2025-10-08 14:02:39 +0200

    fisk i klassen

---
+++ akvarie-iKLassen/js/js.js
@@ -0,0 +1,11 @@
+console.log("Js kører, fedt nok!")
+
+function bang(){
+    //alert("Hva så do?")
+    document.querySelector("#info").innerHTML = `
+        <div>
+            <h2> Goddag med dig </h2>
+            <p>  Jeg er en glad fisk, ja jeg er så. </p>
+        </div>
+    `
+}
